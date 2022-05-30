5406cd1e6e31a282af68d72b4f14b531cabc7d41
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-05-29 22:39:58 -0400

    Version bump because fuck forge gradle

--- Common/src/main/java/me/codexadrian/tempad/mixin/GameRendererMixin.java
+++ Common/src/main/java/me/codexadrian/tempad/mixin/GameRendererMixin.java
@@ -1,21 +1,12 @@
 package me.codexadrian.tempad.mixin;
 
-import com.mojang.blaze3d.shaders.Shader;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import me.codexadrian.tempad.BlurReloader;
 import me.codexadrian.tempad.platform.Services;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.PostChain;
-import net.minecraft.client.renderer.ShaderInstance;
-import net.minecraft.server.packs.resources.ResourceManager;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
