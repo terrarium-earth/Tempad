167e7f156ab7e5bfe526391f13528c021f932a94
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2022-08-02 15:13:25 -0400

    Fixed crashes

--- common/src/main/java/me/codexadrian/tempad/TempadClient.java
+++ common/src/main/java/me/codexadrian/tempad/TempadClient.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class TempadClient {
     private static TempadClientConfig clientConfig;
-    private static final List<String> incompatibleMods = List.of("flywheel", "imm_ptl_core", "structurize");
+    private static final List<String> incompatibleMods = List.of("flywheel", "imm_ptl_core", "structurize", "mahou");
     public static void init() {
         ClampedItemPropertyFunction clampedItemPropertyFunction = (itemStack, clientLevel, livingEntity, i) -> {
             if (livingEntity instanceof Player player && itemStack.getItem() instanceof TempadItem tempad) {
