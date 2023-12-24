adf6bfd5f02005b9fde2e1995bdf567b6a236133
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-12-23 20:54:03 -0500

    2.3.2

--- changelog.md
+++ changelog.md
@@ -1,4 +1,3 @@
-# 2.3.1
+# 2.3.2
 
-- Fix interdimensional travel bug
-- Fix favorited locations ignoring interdimensional travel settings
+- Add null check to Fabric Waystones
--- fabric/src/main/java/me/codexadrian/tempad/common/compat/fabricwaystones/FabricWaystoneLocationGetter.java
+++ fabric/src/main/java/me/codexadrian/tempad/common/compat/fabricwaystones/FabricWaystoneLocationGetter.java
@@ -23,7 +23,7 @@ public class FabricWaystoneLocationGetter {
                 FabricWaystones.WAYSTONE_STORAGE.WAYSTONES
                     .values()
                     .stream()
-                    .filter(waystoneValue -> waystoneValue.isGlobal() || waystoneValue.getEntity().getOwner().equals(player.getUUID()))
+                    .filter(waystoneValue -> waystoneValue.isGlobal() || waystoneValue.getEntity().getOwner() == null || waystoneValue.getEntity().getOwner().equals(player.getUUID()))
                     .forEach(waystoneValue -> {
                         UUID locationId = UUID.nameUUIDFromBytes(waystoneValue.getHash().getBytes());
                         LocationData value = new LocationData(waystoneValue.getWaystoneName(), ResourceKey.create(Registries.DIMENSION, ResourceLocation.tryParse(waystoneValue.getWorldName())), waystoneValue.way_getPos().above(2), locationId, true, false, false);
