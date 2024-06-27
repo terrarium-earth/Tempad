8ccb8e6daa77f6e41cf8da8a0c252020254421dd
Jordan Adams <JordanAdamsBusiness@outlook.com>
2024-06-27 22:58:52 +0000

    Added support for removing more than 1 item from the inventory for teleport cost based on config (#112)

--- common/src/main/java/me/codexadrian/tempad/api/options/impl/ItemOption.java
+++ common/src/main/java/me/codexadrian/tempad/api/options/impl/ItemOption.java
@@ -1,25 +1,52 @@
 package me.codexadrian.tempad.api.options.impl;
 
 import me.codexadrian.tempad.api.options.TempadOption;
+import me.codexadrian.tempad.api.options.TempadOptionApi;
 import me.codexadrian.tempad.common.Tempad;
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemOption extends TempadOption {
+    private int cost = 0;
+
     @Override
     public boolean canTimedoorOpen(Player player, ItemStack stack) {
-        return player.getInventory().contains(Tempad.TEMPAD_FUEL_TAG);
+        cost = TempadOptionApi.getFuelCost(stack);
+        int available = 0;
+
+        for (ItemStack item : findItemStacks(player.getInventory())) {
+            available += item.getCount();
+        }
+
+        return available >= cost;
     }
 
     @Override
     public void onTimedoorOpen(Player player) {
-        findItemStack(player).shrink(1);
+        Inventory inventory = player.getInventory();
+
+        for (ItemStack item : findItemStacks(inventory)) {
+            int count = item.getCount();
+
+            int remainingCost = cost - count;
+            if (remainingCost < 0) {
+                // Logic for removing only a set quantity
+                //  of the item rather than whole itemstack
+                item.setCount(Math.abs(remainingCost));
+                break;
+            }
+
+            cost -= item.getCount();
+            inventory.removeItem(item);
+        }
     }
 
     @Override
@@ -32,17 +59,25 @@ public class ItemOption extends TempadOption {
         return false;
     }
 
-    public ItemStack findItemStack(Player player) {
-        for (ItemStack item : player.getInventory().items) {
-            if (item.is(Tempad.TEMPAD_FUEL_TAG)) {
-                return item;
-            }
-        }
-        for (ItemStack item : player.getInventory().offhand) {
-            if (item.is(Tempad.TEMPAD_FUEL_TAG)) {
-                return item;
-            }
-        }
-        return ItemStack.EMPTY;
+    public List<ItemStack> findItemStacks(Inventory inventory) {
+        NonNullList<ItemStack> playerItems = inventory.items;
+        NonNullList<ItemStack> playerOffhand = inventory.offhand;
+
+        List<ItemStack> items = findItemStacks(playerItems);
+        items.addAll(findItemStacks(playerOffhand));
+
+        return items;
     }
+
+    public List<ItemStack> findItemStacks(NonNullList<ItemStack> itemStacks) {
+        List<ItemStack> items = new ArrayList<>();
+
+        for (ItemStack item : itemStacks) {
+            if (item.is(Tempad.TEMPAD_FUEL_TAG)) {
+                items.add(item);
+            }
+        }
+
+        return items;
+   }
 }
