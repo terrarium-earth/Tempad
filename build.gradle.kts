9fe3bbf56d5a30cb7c36ce330077ed2ab1d007b3
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2025-04-22 11:03:15 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -185,7 +185,7 @@ resourcefulGradle {
             val version: String by project
             val changelog: String = file("changelog.md").readText(Charsets.UTF_8)
             val mrLink: String? = System.getenv("MODRINTH_RELEASE_URL")
-            val cfLink: String? = System.getenv("CURSEFORGE_RELEASE_URL")
+            val cfLink: String? = System.getenv("CURSE_RELEASE_URL")
 
             source.set(file("templates/embed.json.template"))
             injectedValues.set(mapOf(
